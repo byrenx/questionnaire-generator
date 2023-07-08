de88f5fc1ac2b794cbf969a22ec3475ea0ee798d
Jofel <bayron.jofel@gmail.com>
2023-07-08 15:59:46 +0800

    updated to enum and added prettier

--- src/classes/questionnaire.ts
+++ src/classes/questionnaire.ts
@@ -1,26 +1,28 @@
-export type QuestionDifficulty =
-  | "Remembering"
-  | "Understanding"
-  | "Applying"
-  | "Evaluating"
-  | "Creating";
+export enum QuestionDifficulty {
+    REMEMBERING = 'remembering',
+    UNDERSTANDING = 'understanding',
+    APPLYING = 'applying',
+    ANALYZING = 'analyzing',
+    EVALUATING = 'evaluating',
+    CREATING = 'creating'
+}
 
 export interface Question {
-  question: string;
-  difficulty: QuestionDifficulty;
-  choices: string[];
+    question: string;
+    difficulty: QuestionDifficulty;
+    choices: string[];
 }
 
 export class Question {
-  public constructor(data: Partial<Question>) {
-    this.question = data.question || "";
-    this.difficulty = data.difficulty || "Remembering";
-    this.choices = data.choices || [];
-  }
+    public constructor(data: Partial<Question>) {
+        this.question = data.question || '';
+        this.difficulty = data.difficulty || QuestionDifficulty.REMEMBERING;
+        this.choices = data.choices || [];
+    }
 }
 
 export interface QuestionnaireGeneratorConfig {
-  sets: number;
-  itemsPerSet: number;
-  distribution: { [key: string]: number };
+    sets: number;
+    itemsPerSet: number;
+    distribution: { [key: string]: number };
 }
